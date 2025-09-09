a0d16fd05ebf788433493a6ae767a762ed7e62a6
vinnietito <vincentnjuguna69@gmail.com>
2025-09-09 16:40:34 +0300

    Commits

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      console.log("test")
+    </div>
+  )
+}
+
+export default Home
