359080c7a5315fa63789ef6f28b4624794f0df1e
vinnietito <vincentnjuguna69@gmail.com>
2025-09-13 07:57:01 +0300

    Commits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
+//import Cart from './pages/Cart/Cart'
+import Cart from './pages/Cart/Cart.jsx';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
